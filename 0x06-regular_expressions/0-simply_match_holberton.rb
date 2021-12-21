f87922b29eece27086702c844bfbf7e028e6732e
malekmahjoub635 <malouka99@live.com>
2021-12-21 03:47:24 +0000

    fix

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/Holberton/).join
+if ARGV.length != 1
+  puts "we need exactly one argument"
+  exit
+end
+pattren = ARGV[0].scan(/Holberton/)
+for i in pattren
+  print i
+end
+print"\n"
